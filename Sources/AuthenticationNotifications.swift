2257a05020ad844892b14d0a820aaeae334f0d77
Colin T.A. Gray <colinta@gmail.com>
2018-02-13 15:30:02 -0700

    fixes the 'out of date API' notification

--- Sources/AuthenticationNotifications.swift
+++ Sources/AuthenticationNotifications.swift
@@ -5,5 +5,5 @@
 struct AuthenticationNotifications {
     static let userLoggedOut = TypedNotification<Void>(name: "UserElloLoggedOutNotification")
     static let invalidToken = TypedNotification<Void>(name: "ElloInvalidTokenNotification")
-    static let outOfDateAPI = TypedNotification<Void>(name: "ElloInvalidTokenNotification")
+    static let outOfDateAPI = TypedNotification<Void>(name: "ElloOutOfDateAPINotification")
 }
